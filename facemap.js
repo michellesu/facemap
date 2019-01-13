940f5994e4c9f134b4a730c98f44da034da67ae6
Michelle Su <michellesu@umail.ucsb.edu>
2019-01-12 18:06:51 -0800

    sort labels to maps

--- facemap.js
+++ facemap.js
@@ -6,15 +6,55 @@ const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
+// Create map to store descriptions of pictures
+var testMap = new Map();
+
 // Performs label detection on the image file
+var i;
+for(i = 0; i < 3; i++)
+{
 client
-  .labelDetection('cal.jpeg')
+  .labelDetection('cal'+i.toString()+'.jpg')
   .then(results => {
     const labels = results[0].labelAnnotations;
 
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
+    // Store each label description into map
+    // labels.forEach(label => console.log(i + ": " + label.description));
+    labels.forEach(label => 
+    {
+      // console.log("label.description: " + label.description);
+      // console.log("label: " + label);
+      // console.log("labels: " + labels);
+      if(testMap.has(label.description))
+      {
+        var count = testMap.get(label.description);
+        count++;
+        testMap.set(label.description,count);
+      }
+      else
+      {
+        testMap.set(label.description,"1");
+      }
+    });
+
+    // console.log('fun in testMap: ');
+    // console.log('fun in testMap: ' + "fun" in testMap);
+    // console.log('testMap has fun: ' + testMap.has('fun'));
+
+    
+
+    for (var key of testMap.keys()) {
+      console.log(key + ": " + testMap.get(key));
+    }
+
+    // for (var key of testMap.keys()) {
+    //   console.log(key + testMap.has(key));
+    //   console.log(testMap.has(key));
+    //   console.log('----------');
+    // }
   })
   .catch(err => {
     console.error('ERROR:', err);
   });
+}
+
